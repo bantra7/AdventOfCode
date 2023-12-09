f00d9dde055b0f09dfcc27f9bd3b7be4db9c88e8
bantra7 <renault.jbapt@gmail.com>
2023-12-09 13:16:28 +0100

    fix: simplify day 1

--- 2023/day1/main_p1.py
+++ 2023/day1/main_p1.py
@@ -1,18 +1,14 @@
 def main():
-    r = 0
+    score = 0
     with open('1.txt') as f:
         data = f.read().split('\n')
     for line in data:
-        # Récupération du premier digit
-        for c1 in line:
-            if c1.isdigit():
-                break
-        # Récupération du dernier digit
-        for c2 in line[::-1]:
-            if c2.isdigit():
-                break
-        r += int(c1 + c2)
-    print(r)
+        digits = []
+        for c in line:
+            if c.isdigit():
+                digits.append(c)
+        score += int(digits[0] + digits[-1])
+    print(score)
 
 
 if __name__ == '__main__':
--- 2023/day1/main_p2.py
+++ 2023/day1/main_p2.py
@@ -1,50 +1,17 @@
-valid_digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-
-trickys = ['oneight',
-           'twone',
-           'threeight',
-           'fiveight',
-           'eighthree',
-           'nineight']
-
-
 def main():
-    r = 0
+    score = 0
     with open('1.txt') as f:
         data = f.read().split('\n')
     for line in data:
-        new_line_for_first_digit = line
-        new_line_for_last_digit = line
-        # Récupération du premier digit
-        while True:
-            test = [(i, new_line_for_first_digit.find(valid_digit))
-                    for i, valid_digit in enumerate(valid_digits, 1)
-                    if new_line_for_first_digit.find(valid_digit) != -1]
-            if not test:
-                break
-            test.sort(key=lambda x: x[1])
-            valid_digit = test[0][0]
-            new_line_for_first_digit = new_line_for_first_digit.replace(valid_digits[valid_digit - 1],
-                                                                        str(valid_digit))
-        for c1 in new_line_for_first_digit:
-            if c1.isdigit():
-                break
-        # Récupération du dernier digit
-        while True:
-            test = [(i, new_line_for_last_digit.rfind(valid_digit))
-                    for i, valid_digit in enumerate(valid_digits, 1)
-                    if new_line_for_last_digit.rfind(valid_digit) != -1]
-            if not test:
-                break
-            test.sort(key=lambda x: x[1], reverse=True)
-            valid_digit = test[0][0]
-            new_line_for_last_digit = new_line_for_last_digit.replace(valid_digits[valid_digit - 1],
-                                                                      str(valid_digit))
-        for c2 in new_line_for_last_digit[::-1]:
-            if c2.isdigit():
-                break
-        r += int(c1+c2)
-    print(r)
+        digits = []
+        for i, c in enumerate(line):
+            if c.isdigit():
+                digits.append(c)
+            for d, val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'], 1):
+                if line[i:].startswith(val):
+                    digits.append(str(d))
+        score += int(digits[0] + digits[-1])
+    print(score)
 
 
 if __name__ == '__main__':
