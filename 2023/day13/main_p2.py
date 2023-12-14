0a06193c313d4507070ad5d0f74cac0ff4776413
bantra7 <renault.jbapt@gmail.com>
2023-12-14 19:51:05 +0100

    chore: add day 13 part 2

--- 2023/day13/main_p2.py
+++ 2023/day13/main_p2.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 def get_score(m):
-    r = 0
     for i in range(1, len(m)):
         if i < len(m) / 2:
             t1 = m[0:i]
@@ -10,10 +9,14 @@ def get_score(m):
         else:
             t1 = m[::-1][0:len(m) - i]
             t2 = m[::-1][len(m) - i:][0:len(m) - i][::-1]
-        if t1 == t2:
-            r = i
-            break
-    return r
+        total_mismatches = 0
+        for top_row, bottom_row in zip(t1, t2):
+            for (top_char, bottom_char) in zip(top_row, bottom_row):
+                if top_char != bottom_char:
+                    total_mismatches += 1
+        if total_mismatches == 1:
+            return i
+    return 0
 
 
 def main():
@@ -21,7 +24,7 @@ def main():
         data = f.read().split('\n\n')
     hs = 0
     vs = 0
-    for b in data:
+    for b_i, b in enumerate(data, 1):
         m = [c for c in b.split('\n')]
         m1 = [[s for s in l] for l in m]
         mtx = np.array(m1)
